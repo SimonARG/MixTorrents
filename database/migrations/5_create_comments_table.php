e0a9ac7a0d5bf2db364340f8ff4a35b3b9c4789a
Simon_Paul <simonchasnovsky@gmail.com>
2023-10-30 14:36:35 -0300

    Updated migrations

--- database/migrations/5_create_comments_table.php
+++ database/migrations/5_create_comments_table.php
@@ -2,7 +2,6 @@
 
 use App\Models\User;
 use App\Models\Upload;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
